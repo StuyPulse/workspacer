2e56972bf1b5a15d302ddc6f5254f05e04d63eaa
Wilson Berkow <wilsonberkow@gmail.com>
2016-10-25 15:32:15 -0400

    Remove velocity variable in Animation

--- round2/workspace-template/src/Animation.java
+++ round2/workspace-template/src/Animation.java
@@ -2,20 +2,17 @@ import gui.GuiSimple;
 
 public class Animation extends GuiSimple {
     private double yPos;
-    private double velocity;
 
     // This runs when an Animation is created
     public Animation() {
         yPos = 0.0;
-        velocity = 0.0;
     }
 
     public void draw() {
         screen.clear();
         screen.circle(200.0, yPos, 10);
 
-        yPos += velocity;
-        velocity += 1.0;
+        yPos += 1.0;
     }
 
     public static void main(String[] args) {
