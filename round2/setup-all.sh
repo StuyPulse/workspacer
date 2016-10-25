b66a1944370e28053977d324f12f3cc85626de34
Wilson Berkow <wilsonberkow@gmail.com>
2016-10-25 14:45:34 -0400

    chmod group in addition to other

--- round2/setup-all.sh
+++ round2/setup-all.sh
@@ -10,7 +10,7 @@ for i in {2..32}; do
     ssh ${comp} "rm -rf /var/tmp/robo; rm -rf /var/tmp/robo-libs"
     scp -r ~/client2 ${comp}:/var/tmp/robo
     scp -r ~/workspace-libs ${comp}:/var/tmp/robo-libs
-    ssh ${comp} "chmod -R o+r /var/tmp/robo/.ssh && cp /var/tmp/robo/setmeup.sh /var/tmp/setmeup"
+    ssh ${comp} "chmod -R go+r /var/tmp/robo/.ssh && cp /var/tmp/robo/setmeup.sh /var/tmp/setmeup"
 done
 
 # Consider the following, to decentralize the load:
