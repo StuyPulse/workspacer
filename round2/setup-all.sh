cd7481837df86eabc3a83218a2b86028a9e2af2e
Wilson Berkow <wilsonberkow@gmail.com>
2016-10-25 15:17:55 -0400

    Include 149.89.161.1 when setting up on all machines
    Previously hadn't as it couldn't connect. That error was only temporary
    though, and was remedied after a waiting couple seconds.

--- round2/setup-all.sh
+++ round2/setup-all.sh
@@ -1,6 +1,4 @@
-for i in {2..32}; do
-    # TODO: switch the first 3 sections of this to work for cslab4 (currently
-    # this uses the kitchen)
+for i in {1..32}; do
     if [[ $i -lt 10 ]]; then
 	comp=149.89.161.10$i
 	# For cslab1: 149.89.150.10$i
@@ -12,7 +10,3 @@ for i in {2..32}; do
     scp -r ~/workspace-libs ${comp}:/var/tmp/robo-libs
     ssh ${comp} "chmod -R go+r /var/tmp/robo/.ssh && cp /var/tmp/robo/setmeup.sh /var/tmp/setmeup"
 done
-
-# Consider the following, to decentralize the load:
-#self=$HOSTNAME
-#ssh ${comp} "rm -rf /var/tmp/robo && rm -rf /var/tmp/workspace-libs && scp -r $HOSTNAME:/var/tmp/robo /var/tmp/robo && scp -r $HOSTNAME:/var/tmp/workspace-libs /var/tmp/workspace-libs &"
