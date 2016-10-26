8bbdb1f394b19fd6d0737d2ed9f15c88bf4ca35f
Wilson Berkow <wilsonberkow@gmail.com>
2016-10-26 15:04:54 -0400

    Change workspace-template/Makefile to a symbolic link

--- round2/setup-all.sh
+++ round2/setup-all.sh
@@ -5,8 +5,11 @@ for i in {1..32}; do
     else
 	comp=149.89.161.1$i
     fi
-    ssh ${comp} "rm -rf /var/tmp/robo; rm -rf /var/tmp/robo-libs"
+    ssh ${comp} "rm -rf /var/tmp/robo; rm -rf /var/tmp/robo-libs; rm -rf /var/tmp/robo-files"
     scp -r ~/client2 ${comp}:/var/tmp/robo
     scp -r ~/workspace-libs ${comp}:/var/tmp/robo-libs
+    scp -r ~/round2/robo-files ${comp}:/var/tmp/robo-files
     ssh ${comp} "chmod -R go+r /var/tmp/robo/.ssh && cp /var/tmp/robo/setmeup.sh /var/tmp/setmeup"
+
+    echo -e "\nCompleted computer $i of 32\n"
 done
--- round2/setup-here.sh
+++ round2/setup-here.sh
@@ -1,5 +1,6 @@
 comp=localhost
-ssh ${comp} "rm -rf /var/tmp/robo; rm -rf /var/tmp/robo-libs"
+ssh ${comp} "rm -rf /var/tmp/robo; rm -rf /var/tmp/robo-libs; rm -rf /var/tmp/robo-files"
 scp -r ~/client2 ${comp}:/var/tmp/robo
 scp -r ~/workspace-libs ${comp}:/var/tmp/robo-libs
+scp -r ~/round2/robo-files ${comp}:/var/tmp/robo-files
 ssh ${comp} "chmod -R go+r /var/tmp/robo/.ssh && cp /var/tmp/robo/setmeup.sh /var/tmp/setmeup"
