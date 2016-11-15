3548b67527f1ee3ca2b40300f25f4a301090c5d5
Wilson Berkow <wilsonberkow@gmail.com>
2016-11-15 15:40:24 -0500

    Add robotics's pub key to robotics-entry authorized_keys for convenience

--- round2/make-ssh.sh
+++ round2/make-ssh.sh
@@ -1,7 +1,15 @@
 rm -rf ~/client2/.ssh
 mkdir ~/client2/.ssh
+# Make rsa key pair with no password (hence -N "")
 ssh-keygen -t rsa -f ~/client2/.ssh/id_rsa -N ""
 
-# Overwrite authorized_keys with just the new key, on robotics-entry
+# Make an authorized_keys file (named authed-keys) authorizing the new
+# key (from client2, which will be on guest accounts), and also our own key
+cp ~/client2/.ssh/id_rsa.pub authed-keys
+cat ~/.ssh/id_rsa.pub >> authed-keys
+
+# Place that file in .ssh/authorized_keys on robotics-entry
 ssh robotics-entry@localhost "mkdir -p ~/.ssh"
-scp ~/client2/.ssh/id_rsa.pub robotics-entry@localhost:~/.ssh/authorized_keys
+scp authed-keys robotics-entry@localhost:~/.ssh/authorized_keys
+
+rm authed-keys
