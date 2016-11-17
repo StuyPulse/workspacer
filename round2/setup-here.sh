d056eeb68dc414d275d5fd7840209d54e8ce138c
Wilson Berkow <wilsonberkow@gmail.com>
2016-11-17 15:28:02 -0500

    Do not unnecessarily copy robo-libs in setup-here.sh

--- round2/setup-here.sh
+++ round2/setup-here.sh
@@ -1,6 +1,8 @@
 comp=localhost
-ssh ${comp} "rm -rf /var/tmp/robo; rm -rf /var/tmp/robo-libs; rm -rf /var/tmp/robo-files"
+ssh ${comp} "rm -rf /var/tmp/robo; rm -rf /var/tmp/robo-files"
 scp -r ~/client2 ${comp}:/var/tmp/robo
-scp -r ~/workspace-libs ${comp}:/var/tmp/robo-libs
+if [[ ! -d /var/tmp/robo-libs ]]; then
+    scp -r ~/workspace-libs ${comp}:/var/tmp/robo-libs
+fi
 scp -r ~/round2/robo-files ${comp}:/var/tmp/robo-files
 ssh ${comp} "chmod -R go+r /var/tmp/robo/.ssh && cp /var/tmp/robo/setmeup.sh /var/tmp/setmeup"
