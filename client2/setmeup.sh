12ed088864ff2ff6e955365f5b50cdfc96ca3711
Wilson Berkow <wilsonberkow@gmail.com>
2016-11-18 20:17:58 -0500

    Add helpful 'echo's to setmeup.sh

--- client2/setmeup.sh
+++ client2/setmeup.sh
@@ -15,4 +15,16 @@ echo "alias save='/var/tmp/robo/save.sh'" >> ~/.bash_aliases
 echo "alias login='/var/tmp/robo/restore.sh'" >> ~/.bash_aliases
 
 round2path=/home/students/2019/robotics/round2
+
+echo -e "Say 'yes' to the following prompt:\n"
+
 ssh robotics-entry@localhost "ls $round2path/their-work" > ~/.robo-user-list
+
+echo "" # visual separation after SSH stuff
+echo "You're all set up! (If there were errors above, ask for help.)"
+echo "To start work, close this window and open up a new terminal by hitting Ctrl+Alt+T"
+echo "Then, you can log in with the"
+echo " $ login"
+echo "command, or make a new user with the
+echo " $ make-user"
+echo "command."
