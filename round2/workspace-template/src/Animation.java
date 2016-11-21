62f29d52575388f801b6bd844c1808e65a884ff4
Wilson Berkow <wilsonberkow@gmail.com>
2016-11-21 18:22:29 -0500

    Update with work from lesson

--- round2/workspace-template/src/Animation.java
+++ round2/workspace-template/src/Animation.java
@@ -1,51 +1,32 @@
+
+
+
+
+
+
+//That was stupid.
+
 import gui.GuiSimple;
 
-// Animation: Our first class!
-// The comments are a bit excessive, but hopefully they get the ideas across.
 public class Animation extends GuiSimple {
 
-    // Initialize Variables
-    private double xPos;
-    private double yPos;
-    private double xVelocity;
-    private double yVelocity;
+    public Ball wilson;
+    public Ball spalding;
 
     // This runs when an Animation is created
     public Animation() {
-        xPos = 200.0;
-        yPos = 0.0;
-        xVelocity = 1.0;
-        yVelocity = 0.0;
+        wilson = new Ball(2.718, 6.28);
+        spalding = new Ball(3.33,3.43);
     }
 
     // draw(). Is called once every frame
     public void draw() {
-        // clear the screen so that we don't redraw to the same frame
+        //jebron
         screen.clear();
-
-        // screen.circle( x , y , radius )
-        screen.circle(xPos, yPos, 10);
-
-        // "Bounce" when we reach the bottom of the screen
-        if (yPos > 400) {
-            yPos = 400
-            yVelocity *= -1;
-        }
-
-        // Bounce when we reach the sides of the screen
-        if (xPos > 400) {
-            xPos = 400;
-            xVelocity *= -1;
-        } else if (xPos < 0) {
-            xPos = 0;
-            xVelocity *= -1
-        }
-
-        // simulate gravity
-        yVelocity += 0.5;
-
-        xPos += xVelocity;
-        yPos += yVelocity;
+        wilson.update();
+        spalding.update();
+        wilson.render(screen);
+        spalding.render(screen);
     }
 
     // main(). You'll learn what this means later
---
+++ round2/workspace-template/src/Ball.java
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+import gui.Screen;
+
+public class Ball {
+    public double posX;
+    public double posY;
+    public double velX;
+    public double velY;
+
+    public Ball(double vx, double vy) {
+        posX = 200.0;
+        posY = 0.0;
+        
+        velX = vx;
+        velY = vy;
+    }
+
+    public void render(Screen serena) {
+        serena.circle(posX, posY, 10.0);
+    }
+    
+    public void update() {
+        // "Bounce" when we reach the bottom of the screen
+        if (posY > 400) {
+            posY = 400;
+            velY *= -1;
+        }
+
+        // Bounce when we reach the sides of the screen
+        if (posX > 400) {
+            posX = 400;
+            velX *= -1;
+        } else if (posX < 0) {
+            posX = 0;
+            velX *= -1;
+        }
+
+        // simulate gravity
+        velY += 0.5;
+
+        posX += velX;
+        posY += velY;
+    }
+
+}
+
