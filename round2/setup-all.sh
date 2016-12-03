f60b9f145b15b27e4bbde0468351042a335f9b37
Wilson Berkow <wilsonberkow@gmail.com>
2016-12-03 16:13:11 -0500

    Rename setup-all-quick.sh to setup-all.sh (overwrite the latter)

--- round2/setup-all.sh
+++ round2/setup-all.sh
@@ -1,14 +1,18 @@
 for i in {0..32}; do
     if [[ $i -lt 10 ]]; then
-	comp=149.89.161.10$i
-	# For cslab1: 149.89.150.10$i
+        comp=149.89.161.10$i
+        # For cslab1: 149.89.150.10$i
     else
-	comp=149.89.161.1$i
+        comp=149.89.161.1$i
     fi
-    ssh ${comp} "rm -rf /var/tmp/robo; rm -rf /var/tmp/robo-libs; rm -rf /var/tmp/robo-files"
+    ssh ${comp} "rm -rf /var/tmp/robo; rm -rf /var/tmp/robo-files"
     scp -r ~/client2 ${comp}:/var/tmp/robo
-    scp -r ~/workspace-libs ${comp}:/var/tmp/robo-libs
     scp -r ~/round2/robo-files ${comp}:/var/tmp/robo-files
+    if [[ -z $(ssh ${comp} "if [[ -d /var/tmp/robo-libs ]]; then echo yes; fi") ]]; then
+        # If the directory doesn't exist, the ssh output (stdout) will be empty.
+        # Create the directory if it doesn't exist
+        scp -r ~/workspace-libs ${comp}:/var/tmp/robo-libs
+    fi
     ssh ${comp} "chmod -R go+r /var/tmp/robo/.ssh && cp /var/tmp/robo/setmeup.sh /var/tmp/setmeup"
 
     echo -e "\nCompleted computer $i of 32\n"
