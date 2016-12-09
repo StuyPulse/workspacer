40dcaefc90d7c48ef5969d397534287214da1836
Wilson Berkow <wilsonberkow@gmail.com>
2016-12-09 18:05:37 -0500

    Update with work from Dec 9 lesson

--- round2/workspace-template/src/Stats.java
+++ round2/workspace-template/src/Stats.java
@@ -1,24 +1,39 @@
 // Started in Lesson 7 (Dec 1)
 public class Stats {
+
+
+
+
+
     public static void main(String[] args) {
+        double foo = 0.5;
+        double bar = f(foo);
+        foo = bar;
+        foo = 5;
         int[] values = new int[5];
         values[0] = 7;
         values[1] = 3;
         values[2] = 4;
         values[3] = -2;
         values[4] = 12;
-        printArray(values);
-
         int smallest = minimum(values);
+        
+        System.out.println(   f(13)  );
     }
-
+    
     public static int minimum(int[] dataset) {
-        // ... we'll get to this ...
-        return 0;
+        int minSoFar = dataset[0];
+        for (int i = 1; i < dataset.length; i++) {
+            int element = dataset[i];
+            if (element < minSoFar) {
+                minSoFar = element;
+            }
+        }
+        return minSoFar;
     }
-
-    public static double triple(double input) {
-        return input * 3;
+    
+    public static double f(double x) {
+        return 2 * x;
     }
 
     public static void printArray(int[] fred) {
